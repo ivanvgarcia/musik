efab3893b1eb05b47160b809f5a6a364450eec8a
Ivan <ivanvlora@gmail.com>
2019-04-20 20:15:50 +0900

    added the buttons to the now playing bar

--- index.php
+++ index.php
@@ -15,8 +15,47 @@ if (isset($_SESSION['userLoggedIn'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Musik</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-    <h1>Home Page</h1>
+    <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+            <div id="nowPlayingLeft">
+
+            </div>
+            <div id="nowPlayingCenter">
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class="controlButton shuffle" title="Shuffle button">
+                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                        </button>
+
+                        <button class="controlButton previous" title="Previous button">
+                            <img src="assets/images/icons/previous.png" alt="previous">
+                        </button>
+
+                        <button class="controlButton play" title="Play button">
+                            <img src="assets/images/icons/play.png" alt="play">
+                        </button>
+
+                        <button class="controlButton pause" title="Pause button">
+                            <img src="assets/images/icons/pause.png" alt="pause">
+                        </button>
+
+                        <button class="controlButton next" title="Next button">
+                            <img src="assets/images/icons/next.png" alt="next">
+                        </button>
+
+                        <button class="controlButton repeat" title="Repeat button">
+                            <img src="assets/images/icons/repeat.png" alt="Repeat">
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div id="nowPlayingRight">
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
