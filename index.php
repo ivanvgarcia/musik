ee5beeda069fa9306fb53806d77db64812263836
Ivan <ivanvlora@gmail.com>
2019-04-21 13:08:36 +0900

    styled the side navbar and created partials for the different components

--- index.php
+++ index.php
@@ -24,80 +24,12 @@ if (isset($_SESSION['userLoggedIn'])) {
     <div id="mainContainer">
 
         <div id="topContainer">
-            <div id="navBarContainer">
-
-            </div>
+            <?php include "includes/navBarContainer.php";?>
+            <div id="mainViewContainer"></div>
         </div>
 
         <div id="nowPlayingBarContainer">
-            <div id="nowPlayingBar">
-                <div id="nowPlayingLeft">
-                    <div class="content">
-                        <span class="albumLink">
-                            <img src="assets/images/prfive.png" alt="">
-                        </span>
-                        <div class="trackInfo">
-                            <span class="trackName">
-                                <span>Darte Un Beso</span>
-                            </span>
-                            <span class="artistName">
-                                <span>Prince Royce</span>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div id="nowPlayingCenter">
-                    <div class="content playerControls">
-                        <div class="buttons">
-                            <button class="controlButton shuffle" title="Shuffle button">
-                                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                            </button>
-
-                            <button class="controlButton previous" title="Previous button">
-                                <img src="assets/images/icons/previous.png" alt="previous">
-                            </button>
-
-                            <button class="controlButton play" title="Play button">
-                                <img src="assets/images/icons/play.png" alt="play">
-                            </button>
-
-                            <button class="controlButton pause" title="Pause button">
-                                <img src="assets/images/icons/pause.png" alt="pause">
-                            </button>
-
-                            <button class="controlButton next" title="Next button">
-                                <img src="assets/images/icons/next.png" alt="next">
-                            </button>
-
-                            <button class="controlButton repeat" title="Repeat button">
-                                <img src="assets/images/icons/repeat.png" alt="Repeat">
-                            </button>
-                        </div>
-
-                        <div class="playbackBar">
-                            <span class="progressTime current">0.00</span>
-                            <div class="progressBar">
-                                <div class="progressBarBg">
-                                    <div class="progress"></div>
-                                </div>
-                            </div>
-                            <span class="progressTime remaining">0.00</span>
-                        </div>
-                    </div>
-                </div>
-                <div id="nowPlayingRight">
-                    <div class="volumeBar">
-                        <button class="controlButton volume" title="Volume button">
-                            <img src="assets/images/icons/volume.png" alt="volume">
-                        </button>
-                        <div class="progressBar">
-                            <div class="progressBarBg">
-                                <div class="progress"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php include "includes/nowPlayingBarContainer.php";?>
         </div>
     </div>
 </body>
